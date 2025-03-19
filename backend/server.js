edaa1127c2ee496ab12e42127f44755bd7442d79
beauredj <joseph.beau-reder@univ-grenoble-alpes.fr>
2025-03-19 10:17:50 +0100

    change port

--- backend/server.js
+++ backend/server.js
@@ -97,7 +97,7 @@ app.get('/conversations', async (req, res) => {
 });
 
 // Server start
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
